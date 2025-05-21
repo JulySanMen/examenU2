c083a6eb2a96f628a2b1ef2a01b0f2d0dc67e04b
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-20 20:32:52 -0600

    feat: add manejarCambio function to update genero state in Filtros component

--- src/Components/Filtros.jsx
+++ src/Components/Filtros.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 const Filtros = ({onFilter}) => {
   const [genero,setGenero] = useState('');
+
+  const manejarCambio = (e) => {
+    setGenero(e.target.value);
   return (
     <div>Filtros</div>
 
