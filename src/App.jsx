d0dea2e9a78797790b9a4eb77be16eb6d055bdbd
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-20 20:37:51 -0600

    feat: add Filtros component to App for improved user interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Formulario from './Components/Formulario'
+import Filtros from './Components/Filtros'
 
 const App = () => {
   return (
     <>
-    </>
+    <Filtros/>
     <Formulario/>
     </>
   )
